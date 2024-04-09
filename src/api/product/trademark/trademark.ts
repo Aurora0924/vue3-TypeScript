76c2a3d9d74a3d90bc2905d42af2476106a8f2e4
ynzhoushiwei <ynzhoushiwei@gmail.com>
2024-04-09 16:07:38 +0800

    feat: 完成了品牌添加页面的静态搭建

--- src/api/product/trademark/trademark.ts
+++ src/api/product/trademark/trademark.ts
@@ -1,10 +1,24 @@
 import request from "@/utils/request";
-import type { TrademarkResponseData } from "@/api/product/model/trademarkModel";
+import type { TrademarkResponseData,responseTrademark } from "@/api/product/model/trademarkModel";
 enum API {
-  TRAEDMARK_URL = "/admin/product/baseTrademark/",
+  TRADEMARK_URL = "/admin/product/baseTrademark/",
+  SAVE_TRADEMARK_URL = '/admin/product/baseTrademark/save',
+  UPDATE_TRADEMARK_URL = '/admin/product/baseTrademark/update'
 }
 
 export const reqHasTrademark = (page: number, limit: number) =>
   request.get<any, TrademarkResponseData>(
-    API.TRAEDMARK_URL + `${page}/${limit}`,
+    API.TRADEMARK_URL + `${page}/${limit}`,
   );
+
+//添加与修改已有品牌接口方法
+export const reqAddOrUpdateTrademark = (data: responseTrademark) => {
+ //修改已有品牌的数据
+ if (data.id) {
+  return request.put<any, any>(API.UPDATE_TRADEMARK_URL, data)
+  } else {
+    //新增品牌
+    return request.post<any, any>(API.SAVE_TRADEMARK_URL, data)
+  }
+}
+ 
