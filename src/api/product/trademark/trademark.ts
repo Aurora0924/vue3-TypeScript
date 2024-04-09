01929c0d49611beb259e5cb627f107f2e58431ce
ynzhoushiwei <ynzhoushiwei@gmail.com>
2024-04-09 16:07:49 +0800

    feat: 完成了品牌添加页面的静态搭建

--- src/api/product/trademark/trademark.ts
+++ src/api/product/trademark/trademark.ts
@@ -1,9 +1,12 @@
 import request from "@/utils/request";
-import type { TrademarkResponseData,responseTrademark } from "@/api/product/model/trademarkModel";
+import type {
+  TrademarkResponseData,
+  responseTrademark,
+} from "@/api/product/model/trademarkModel";
 enum API {
   TRADEMARK_URL = "/admin/product/baseTrademark/",
-  SAVE_TRADEMARK_URL = '/admin/product/baseTrademark/save',
-  UPDATE_TRADEMARK_URL = '/admin/product/baseTrademark/update'
+  SAVE_TRADEMARK_URL = "/admin/product/baseTrademark/save",
+  UPDATE_TRADEMARK_URL = "/admin/product/baseTrademark/update",
 }
 
 export const reqHasTrademark = (page: number, limit: number) =>
@@ -13,12 +16,11 @@ export const reqHasTrademark = (page: number, limit: number) =>
 
 //添加与修改已有品牌接口方法
 export const reqAddOrUpdateTrademark = (data: responseTrademark) => {
- //修改已有品牌的数据
- if (data.id) {
-  return request.put<any, any>(API.UPDATE_TRADEMARK_URL, data)
+  //修改已有品牌的数据
+  if (data.id) {
+    return request.put<any, any>(API.UPDATE_TRADEMARK_URL, data);
   } else {
     //新增品牌
-    return request.post<any, any>(API.SAVE_TRADEMARK_URL, data)
+    return request.post<any, any>(API.SAVE_TRADEMARK_URL, data);
   }
-}
- 
+};
--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,8 +1,20 @@
 <template>
   <el-card class="box-card">
-    <el-button type="primary" size="default" icon="Plus" @click="addTrademark" v-has="`btn.Trademark.add`">添加品牌</el-button>
+    <el-button
+      type="primary"
+      size="default"
+      icon="Plus"
+      @click="addTrademark"
+      v-has="`btn.Trademark.add`"
+      >添加品牌</el-button
+    >
     <el-table border style="margin: 10px 0px" :data="trademarkArr">
-      <el-table-column label="序号" align="center" width="80px" type="index"></el-table-column>
+      <el-table-column
+        label="序号"
+        align="center"
+        width="80px"
+        type="index"
+      ></el-table-column>
       <el-table-column label="品牌名称" align="center">
         <template #="{ row, $index }">
           <pre>{{ row.tmName }}</pre>
@@ -15,8 +27,18 @@
       </el-table-column>
       <el-table-column label="品牌操作" align="center">
         <template #="{ row, $index }">
-          <el-button type="primary" size="small" icon="Edit" @click="updateTrademark"></el-button>
-          <el-popconfirm :title="`您确定要删除${row.tmName}?`" width="250px" icon="Delete" @confirm="">
+          <el-button
+            type="primary"
+            size="small"
+            icon="Edit"
+            @click="updateTrademark"
+          ></el-button>
+          <el-popconfirm
+            :title="`您确定要删除${row.tmName}?`"
+            width="250px"
+            icon="Delete"
+            @confirm=""
+          >
             <template #reference>
               <el-button type="primary" size="small" icon="Delete"></el-button>
             </template>
@@ -24,15 +46,38 @@
         </template>
       </el-table-column>
     </el-table>
-    <el-pagination @size-change="sizeChange" @current-change="getTrademark" v-model:current-page="pageNo" v-model:page-size="limit" :page-sizes="[1, 3, 5, 7, 9]" :background="true" layout="prev, pager, next, jumper,->,sizes,total" :total="total" />
+    <el-pagination
+      @size-change="sizeChange"
+      @current-change="getTrademark"
+      v-model:current-page="pageNo"
+      v-model:page-size="limit"
+      :page-sizes="[1, 3, 5, 7, 9]"
+      :background="true"
+      layout="prev, pager, next, jumper,->,sizes,total"
+      :total="total"
+    />
   </el-card>
-  <el-dialog v-model="dialogVisible" title="添加品牌" width="500" :before-close="handleClose">
-    <el-from style="width: 80%;">
+  <el-dialog
+    v-model="dialogVisible"
+    title="添加品牌"
+    width="500"
+    :before-close="handleClose"
+  >
+    <el-from style="width: 80%">
       <el-form-item label="品牌名称" label-width="100px">
-        <el-input placeholder="请您输入品牌名称" v-model="trademarkParams.tmName"></el-input>
+        <el-input
+          placeholder="请您输入品牌名称"
+          v-model="trademarkParams.tmName"
+        ></el-input>
       </el-form-item>
       <el-form-item label="品牌LOGO" label-width="100px">
-        <el-upload class="avatar-uploader" action="/api/admin/product/fileUpload" :show-file-list="false" :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+        <el-upload
+          class="avatar-uploader"
+          action="/api/admin/product/fileUpload"
+          :show-file-list="false"
+          :on-success="handleAvatarSuccess"
+          :before-upload="beforeAvatarUpload"
+        >
           <img v-if="imageUrl" :src="imageUrl" class="avatar" />
           <el-icon v-else class="avatar-uploader-icon">
             <Plus />
@@ -48,46 +93,53 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, reactive } from 'vue'
-import { reqHasTrademark } from '@/api/product/trademark/trademark'
-import type { responseTrademarkList, TrademarkResponseData, responseTrademark } from '@/api/product/trademark/type'
-const pageNo = ref<number>(1)
-const limit = ref<number>(6)
-const total = ref<number>(0)
-const dialogVisible = ref<boolean>(false)
-const trademarkArr = ref<responseTrademarkList>([])
+import { ref, onMounted, reactive } from "vue";
+import { reqHasTrademark } from "@/api/product/trademark/trademark";
+import type {
+  responseTrademarkList,
+  TrademarkResponseData,
+  responseTrademark,
+} from "@/api/product/trademark/type";
+const pageNo = ref<number>(1);
+const limit = ref<number>(6);
+const total = ref<number>(0);
+const dialogVisible = ref<boolean>(false);
+const trademarkArr = ref<responseTrademarkList>([]);
 // 封装函数
 const trademarkParams = reactive<responseTrademark>({
-  tmName: '',
-  logoUrl: ''
-})
+  tmName: "",
+  logoUrl: "",
+});
 const getTrademark = async (pager = 1) => {
-  pageNo.value = pager
-  let request: TrademarkResponseData = await reqHasTrademark(pageNo.value, limit.value)
+  pageNo.value = pager;
+  let request: TrademarkResponseData = await reqHasTrademark(
+    pageNo.value,
+    limit.value,
+  );
 
   if (request.code == 200) {
-    total.value = request.data.total
-    trademarkArr.value = request.data.records
+    total.value = request.data.total;
+    trademarkArr.value = request.data.records;
   }
-}
+};
 const updateTrademark = () => {
-  dialogVisible.value = true
-}
+  dialogVisible.value = true;
+};
 const addTrademark = () => {
-  dialogVisible.value = true
-}
+  dialogVisible.value = true;
+};
 const cencal = () => {
-  dialogVisible.value = false
-}
+  dialogVisible.value = false;
+};
 const confirm = () => {
-  dialogVisible.value = false
-}
+  dialogVisible.value = false;
+};
 onMounted(() => {
-  getTrademark()
-})
+  getTrademark();
+});
 const sizeChange = () => {
-  getTrademark()
-}
+  getTrademark();
+};
 </script>
 
 <style lang="scss" scoped>
